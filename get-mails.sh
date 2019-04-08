f3dd64d1b7e14e71fac16282e08f7127171c9743
Marcos Marado <mindboosternoori@gmail.com>
2019-04-08 14:40:32 +0100

    make the script work with the updated website
    Closes: #2

--- get-mails.sh
+++ get-mails.sh
@@ -1,36 +1,19 @@
 #!/bin/bash
 
-# change this to the URL of the committee you want to get e-mail addresses for
-url="http://www.europarl.europa.eu/meps/en/full-list.html?filter=all&leg="
+url="http://www.europarl.europa.eu/meps/en/full-list/xml"
 
-DEBUG=false
+wget $url
+xml2csv --input xml --output csv --tag mep
 
-# get the pages
-rm -rf debug *html
-wget "$url" -o /dev/null -O "meps.html";
+echo "$(head -n1 csv),email" > emails;
 
-# get the MEP page URLs
-grep  href=\"/meps meps.html |grep _home|grep -v \>|cut -d\" -f2 > meps-urls
-
-# get the MEP pages
-if [ "$DEBUG" != true ]; then
-	rm *html
-else
-	mkdir debug; mv *html debug
-fi
-for m in $(cat meps-urls); do
-	wget "http://www.europarl.europa.eu$m" -o /dev/null;
-done
-
-# get the email addresses from the MEP pages
-echo "e-mail;Country" > emails;
-for i in *html; do
-	email=$(grep mailto $i|cut -d\" -f2|cut -d: -f2-|rev|sed 's/\]ta\[/@/g'|sed 's/\]tod\[/\./g'|head -n 1);
-	country=$(grep nationality $i|cut -d\> -f2);
-	echo "$email;$country" >> emails;
-done;
+list=$(tail -n $(($(cat csv|wc -l) - 1)) csv)
+while read -r line; do
+	id=$(echo "$line"|cut -d\" -f8)
+	email=$(wget "http://www.europarl.europa.eu/meps/en/$id" -o /dev/null -O -|grep mailto|cut -d: -f2-|rev|sed 's/\]ta\[/@/g'|sed 's/\]tod\[/\./g'|head -n 1|cut -d\" -f8)
+	#echo "$email"
+	echo "$line,\"$email\"" >> emails
+done <<< "$list"
 
 # cleanup
-if [ "$DEBUG" != true ]; then
-	rm *html
-fi
+rm xml csv
