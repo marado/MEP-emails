ae7f2b8d3a674b0ee5d469b2ce27c0ff69f08cd1
Marcos Marado <mindboosternoori@gmail.com>
2022-12-07 23:25:02 +0000

    update the script to make it work again
    * the pages are now served over https
    * the format where the email is defined is even more obfuscated (no
      longer a mailto:)

--- get-mails.sh
+++ get-mails.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-url="http://www.europarl.europa.eu/meps/en/full-list/xml"
+url="https://www.europarl.europa.eu/meps/en/full-list/xml"
 
 wget $url
+
+# TODO: fail gracefully if we don't have xml2csv installed
 xml2csv --input xml --output csv --tag mep
 
 echo "$(head -n1 csv),email" > emails;
@@ -10,8 +12,8 @@ echo "$(head -n1 csv),email" > emails;
 list=$(tail -n $(($(cat csv|wc -l) - 1)) csv)
 while read -r line; do
 	id=$(echo "$line"|cut -d\" -f8)
-	email=$(wget "http://www.europarl.europa.eu/meps/en/$id" -o /dev/null -O -|grep mailto|cut -d: -f2-|rev|sed 's/\]ta\[/@/g'|sed 's/\]tod\[/\./g'|head -n 1|cut -d\" -f8)
-	#echo "$email"
+	email=$(wget "https://www.europarl.europa.eu/meps/en/$id" -o /dev/null -O -|grep link_email|cut -d\" -f4|rev|sed 's/\]ta\[/@/g'|sed 's/\]tod\[/\./g'|head -n 1|cut -d\" -f8)
+	echo "$email"
 	echo "$line,\"$email\"" >> emails
 done <<< "$list"
 
